1a520981630f01b69369a5bb1b98201daf126b75
bill <290971614@qq.com>
2017-07-21 17:40:00 +0800

    code

--- Finger/Finger/HomeVC.m
+++ Finger/Finger/HomeVC.m
@@ -11,6 +11,7 @@
 #import "LoginVC.h"
 
 @interface HomeVC ()
+@property (strong, nonatomic) IBOutlet UIWebView *myWebview;
 
 @end
 
@@ -18,7 +19,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    // 2.创建请求
+    NSMutableURLRequest *request =[NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://help.kaiyuan.net"]];
+    // 3.加载网页
+    [self.myWebview loadRequest:request];
     
     //由storyboard根据myView的storyBoardID来获取我们要切换的视图
     if (![UserInfo sharedInstance].isLogin) {
